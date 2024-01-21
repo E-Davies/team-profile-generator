0c782b0ece523e6370f7a03da9ca231b8fdec799
e-davies <emmi.lou002@gmail.com>
2024-01-21 16:14:23 +0000

    instantiate classes within each inquire function

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-// const Manager = require("./lib/Manager");
-// const Engineer = require("./lib/Engineer");
-// const Intern = require("./lib/Intern");
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
 const questions = require("./lib/questions.js");
 const inquirer = require("inquirer");
 const path = require("path");
@@ -14,9 +14,10 @@ const render = require("./src/page-template.js");
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
-let manager = [];
-let engineer = [];
-let intern = [];
+let team = [];
+// let manager = [];
+// let engineer = [];
+// let intern = [];
 let id = [];
 
 
@@ -26,7 +27,8 @@ const init = () => {
         .prompt(questions.managerQuestions)  
         .then((answers) =>{ 
             // console.log(answers)
-            manager.push(answers);
+            const manager = new Manager(answers.managersName, answers.managersID, answers.managersEmail, answers.managersOfficeNumber);
+            team.push(manager);
             id.push(answers.managersID);
             addAnotherEmployee();
         });
@@ -43,37 +45,38 @@ const addAnotherEmployee = () => {
             addInternQuestions()
         }else{
             console.log('Building team complete')
-            console.log(manager);
-            console.log(engineer);
-            console.log(intern);
+            console.log(team);
+            // console.log(manager);
+            // console.log(engineer);
+            // console.log(intern);
             console.log(id);
             // Generate classes?
             
-            console.log('*********************************************');
-            console.log(`Manager's name is: ${manager[0].managersName}`);
-            console.log(`Engineer 2's Name is: ${engineer[1].engineersName}`);
-            console.log(`Engineer 2's ID is: ${engineer[1].engineersID}`);
+            // console.log('*********************************************');
+            // console.log(`Manager's number is: ${team[0].getOfficeNumber()}`);
+            // console.log(team[0].getRole());
+            // console.log('                                             ');
+            // console.log(`Manager's role is: ${team[0].getRole()}`);
+            // console.log(team[1].getRole());
 
-            
-            
-            //what to do with the answers collated from the user?
-            //writeToFile(manager, engineer, intern);
+            // console.log(`Engineer 2's Name is: ${engineer[1].engineersName}`);
+            // console.log(`Engineer 2's ID is: ${engineer[1].engineersID}`);
+           
+            //Func call to use the team array (that's been created based on user input) to create a HTML file
+            writeToFile(team);
         };
     });
 };
 
 const addEngineerQuestions = () => {
-    // let count = 1;
     inquirer
     .prompt(questions.engineerQuestions)  
     .then((answers) =>{ 
-        console.log(answers);
-        engineer.push(answers);
+        // console.log(answers);
+        const engineer = new Engineer(answers.engineersName, answers.engineersID, answers.engineersEmail, answers.engineersGithub);
+        team.push(engineer);
         id.push(answers.engineersID);
-        // or generate class i.e. const engineer[1] = new Engineer(answers.engineersGithub);?
-        
-        // count++;
-        addAnotherEmployee()
+        addAnotherEmployee();
     });
 };
 
@@ -81,16 +84,17 @@ const addInternQuestions = () => {
     inquirer
         .prompt(questions.internQuestions)  
         .then((answers) =>{ 
-            console.log(answers)
-            intern.push(answers);
+            // console.log(answers);
+            const intern = new Intern(answers.internsName, answers.internsID, answers.internsEmail, answers.internsSchool); 
+            team.push(intern);
             id.push(answers.internsID);
-
-            addAnotherEmployee()
+            addAnotherEmployee();
         });         
 };
 
-function writeToFile(manager, engineer, intern){
-    fs.writeFile('./output/team.html', render(manager, engineer, intern), (err) => 
+//Func to use the team array (that's been created based on user input) to create a HTML file
+function writeToFile(team){
+    fs.writeFile('./output/team.html', render(team), (err) => 
     err ? console.error(err) : console.log("Success! You're team page has been created. You will find the team.html file in the output folder."))
 };
 
